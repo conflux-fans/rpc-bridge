71209c72794c0bbe360ef4e5f264ae27a0e40531
PanaW <wangpan@conflux-chain.org>
2021-08-04 16:19:27 +0800

    update networkId init logic

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const { JsonRpcEngine } = require('json-rpc-engine');
 const { Conflux } = require('js-conflux-sdk');
+const utils = require('./utils');
 
 const CONFIG = require('./config.json');
 const CFXProvider = require('./middlewares/cfxProvider');
@@ -10,13 +11,14 @@ const jsonrpcLogger = require('./middlewares/logger');
 const adaptGasPrice = require('./middlewares/adaptGasPrice');
 // const ETHProvider = require('./middlewares/ethProvider');
 // const mapTxHash = require('./middlewares/mapTxHash');
-// const adaptGetCodeResponse = require('./middlewares/adaptGetCodeResponse');
-
 
 async function startApp() {
-  const cfx = new Conflux(CONFIG);
-  const { networkId } = await cfx.getStatus();
-  const engine = createMiddlewareEngine(CONFIG.url, networkId);
+  const networkId = await getNetworkId(CONFIG.url);
+  if (!networkId) {
+    console.log('Can not connect to Conflux RPC');
+    return;
+  };
+  const engine = getMiddlewareEngine(CONFIG.url, networkId);
 
   const app = new Koa();
   app.use(bodyParser());
@@ -30,10 +32,11 @@ async function startApp() {
     console.error('server error', err);
   });
 
+  console.log('******** Starting the RPC-bridge server ********');
   app.listen(CONFIG.port);
 }
 
-function createMiddlewareEngine(url, networkId) {
+function getMiddlewareEngine(url, networkId) {
   let engine = new JsonRpcEngine();
   engine.push(adaptGasPrice());
   engine.push(jsonrpcLogger());
@@ -43,6 +46,24 @@ function createMiddlewareEngine(url, networkId) {
   engine.push(CFXProvider({url, networkId}));
   // engine.push(ETHProvider({url: 'http://localhost:7585'}))
   return engine;
+}
+
+async function getNetworkId(url) {
+  const cfx = new Conflux({url});
+  let networkId
+  let n = 10;
+  while(!networkId && n > 0) {
+    try {
+      const status = await cfx.getStatus();
+      networkId = status.networkId;
+    } catch(err) {
+      console.log('******** Retry to get networkId');
+      await utils.waitNS(5);
+    }
+    n--;
+  }
+
+  return networkId;
 }
 
 startApp().catch(console.log);
--- utils.js
+++ utils.js
@@ -13,7 +13,12 @@ function hasCFXAddressNamespace(address) {
   return ['0x0', '0x1', '0x8'].indexOf(namespace) > -1;
 }
 
+async function waitNS(number = 30) {
+  await new Promise((resolve, reject) => {setTimeout(resolve, number * 1000)});
+}
+
 module.exports = {
   ethAddressToCfx,
   ethContractAddressToCfx,
+  waitNS,
 };
