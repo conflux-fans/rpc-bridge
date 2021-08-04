247b4e71bef2a1f2e8b22f990f4c0dcd9f990d41
PanaW <wangpan@conflux-chain.org>
2021-08-04 14:53:09 +0800

    update config.sample

--- middlewares/adaptGasPrice.js
+++ middlewares/adaptGasPrice.js
@@ -3,7 +3,7 @@ module.exports = function() {
   return createAsyncMiddleware(async (req, res, next) => {
     const { method } = req;
     if (method === 'eth_gasPrice') {
-      res.result = '0x3b9aca00';
+      res.result = '0x3b9aca00';   // 1Gwei
       return;
     }
     await next();
