1bcea2ebdfd2ef4e43423830dd210053433c37e5
PanaW <pana.wang@outlook.com>
2023-09-14 15:35:22 +0800

    optimize block number adapt method

--- middlewares/adaptBlockNumber.js
+++ middlewares/adaptBlockNumber.js
@@ -1,8 +1,10 @@
 const { createAsyncMiddleware } = require('json-rpc-engine');
+
 module.exports = function() {
   return createAsyncMiddleware(async (req, res, next) => {
     const { method } = req;
-    if (method === 'eth_getBlockByNumber' && req.params[0] === 'latest') {
+    // adapt default block number tag to safe
+    if (method === 'eth_getBlockByNumber' && (req.params[0] === 'latest' || req.params.length === 0)) {
         req.params[0] = 'safe';
     }
     await next();
